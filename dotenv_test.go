04816bf61151c8a07895fe557b9e17f9ff1ec46c
Corey Prophitt <git@prophitt.me>
2022-12-31 07:32:33 -0800

    Added test file and test case

--- dotenv_test.go
+++ dotenv_test.go
@@ -1,1 +1,22 @@
 package dotenv
+
+import (
+	"os"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	ScanFiles = []string{"testfiles/env"}
+
+	if err := Load(); err != nil {
+		t.Fatal("failed to load testfiles/env")
+	}
+
+	if os.Getenv("DOTENV_ENV") != "development" {
+		t.Fatalf("failed to set DOTENV_ENV; got %s", os.Getenv("DOTENV_ENV"))
+	}
+
+	if os.Getenv("TEST_KEY") != "abc-123" {
+		t.Fatalf("failed to set TEST_KEY; got %s", os.Getenv("TEST_KEY"))
+	}
+}
