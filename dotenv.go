e4d30a0b7ebe14df8ad9cca4b02305ad875c00c4
Corey Prophitt <git@prophitt.me>
2022-12-31 07:32:21 -0800

    Updated interface

--- dotenv.go
+++ dotenv.go
@@ -1,13 +1,21 @@
 // Package dotenv loads local .env files into the environment.
 package dotenv
 
+import "fmt"
+
 var (
-	// ScanPath are the directories that will be scanned for .env files.
-	ScanPath = []string{"./"}
+	// ScanFiles a slice of files to look for. Each will be loaded.
+	ScanFiles = []string{".env"}
 )
 
 // Load scans the local environment for .env files and loads each one that is
 // found.
 func Load() error {
+	for _, file := range ScanFiles {
+		fmt.Println(file)
+
+		// TODO: Load file.
+	}
+
 	return nil
 }
