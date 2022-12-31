0b5c5af4acef8367270a9becc867cb9799f16214
Corey Prophitt <git@prophitt.me>
2022-12-31 07:01:02 -0800

    Stubbed dotenv.go

--- dotenv.go
+++ dotenv.go
@@ -1,1 +1,13 @@
+// Package dotenv loads local .env files into the environment.
 package dotenv
+
+var (
+	// ScanPath are the directories that will be scanned for .env files.
+	ScanPath = []string{"./"}
+)
+
+// Load scans the local environment for .env files and loads each one that is
+// found.
+func Load() error {
+	return nil
+}
