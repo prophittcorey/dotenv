807c6b7e9ff42ab1b39bc8e3b8ebf31ce7b90677
Corey Prophitt <git@prophitt.me>
2023-01-01 06:05:40 -0800

    Updated tests

--- dotenv_test.go
+++ dotenv_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestLoad(t *testing.T) {
-	ScanFiles = []string{"testfiles/env"}
+	ScanFiles = []string{"testfiles/env", "testfiles/env-alt"}
 
 	if err := Load(); err != nil {
 		t.Fatal("failed to load testfiles/env")
@@ -16,7 +16,11 @@ func TestLoad(t *testing.T) {
 		t.Fatalf("failed to set DOTENV_ENV; got %s", os.Getenv("DOTENV_ENV"))
 	}
 
-	if os.Getenv("TEST_KEY") != "abc-123" {
+	if os.Getenv("TEST_KEY") != "123-abc" {
 		t.Fatalf("failed to set TEST_KEY; got %s", os.Getenv("TEST_KEY"))
+	}
+
+	if os.Getenv("ALT_ENV") != "abc-def" {
+		t.Fatalf("failed to set ALT_ENV; got %s", os.Getenv("ALT_ENV"))
 	}
 }
